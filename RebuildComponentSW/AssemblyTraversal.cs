a5451b24ff8600b5126fc85ae6ab03ed257204b8
Vladislav07 <belov_vlad@list.ru>
2024-12-18 17:41:01 +0300

    1

---
+++ RebuildComponentSW/AssemblyTraversal.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using SolidWorks.Interop.sldworks;
+using SolidWorks.Interop.swconst;
+using System.IO;
+using System.Text.RegularExpressions;
+
+
+namespace RebuildComponentSW
+{
+   public  class AssemblyTraversal
+    {
+        List<FileRef> list;
+        ModelDoc2 swModel;
+       
+        public AssemblyTraversal(ModelDoc2 m)
+        {
+            swModel = m;
+        }
+
+        public void Main()
+        {
+          
+            Configuration swConf;
+            Component2 swRootComp;
+            FileRef rootComp;
+            string rootName;
+            string nameCuby; 
+            list = new List<FileRef>();
+            swConf = (Configuration)swModel.GetActiveConfiguration();
+            swRootComp = (Component2)swConf.GetRootComponent();
+            rootName = swModel.GetPathName();
+            nameCuby = Path.GetFileNameWithoutExtension(rootName);
+            rootComp = new FileRef(nameCuby, rootName);
+            rootComp.Description = ".SLDASM";
+            TraverseComponent(swRootComp, 1, ref rootComp);
+            ListFormationToRebuild(rootComp);
+
+           // swApp.CloseAllDocuments(true);    
+           // OpenAndRefresh();
+            // pdm.DrawingsBatchUnLock();
+
+        }
+
+        public void TraverseComponent(Component2 swComp, int nLevel, ref FileRef fr)
+        {
+            object[] ChildComps;
+            Component2 swChildComp;
+            FileRef childFileRef;
+            string childName;
+            string e;
+            string designation;
+
+            ChildComps = (object[])swComp.GetChildren();
+
+            for (int i = 0; i < ChildComps.Length; i++)
+            {
+                swChildComp = (Component2)ChildComps[i];
+                if (!swChildComp.IsSuppressed()) continue;
+                childName = swChildComp.GetPathName();
+                e = Path.GetExtension(childName);
+                designation = Path.GetFileNameWithoutExtension(childName);
+                string regCuby = @"^CUBY-\d{8}$";
+                bool IsCUBY = Regex.IsMatch(designation, regCuby);
+                if (!IsCUBY) continue;
+
+                childFileRef = new FileRef(designation, childName);
+                childFileRef.Description = e;
+                childFileRef.ComponentLevel = nLevel.ToString();
+                fr.FileRefs.Add(childFileRef);
+                TraverseComponent(swChildComp, nLevel + 1, ref childFileRef);
+               
+                // Debug.Print(sPadStr + swChildComp.Name2 + " <" + swChildComp.ReferencedConfiguration + ">");
+            }
+        }
+
+        public void OpenAndRefresh()
+        {
+            ModelDoc2 swModelDoc = default(ModelDoc2);
+            int errors = 0;
+            int warnings = 0;
+            int lErrors = 0;
+            int lWarnings = 0;
+            string fileName = null;
+            // list.Sort();
+            try
+            {
+                foreach (FileRef item in list)
+                {
+                    fileName = item.PathName;
+                    if (item.Description == ".SLDASM" || item.Description == ".sldasm")
+                    {
+                        swModelDoc = (ModelDoc2)swApp.OpenDoc6(fileName, (int)swDocumentTypes_e.swDocASSEMBLY, (int)swOpenDocOptions_e.swOpenDocOptions_Silent, "", ref errors, ref warnings);
+                    }
+                    else
+                    {
+                        swModelDoc = (ModelDoc2)swApp.OpenDoc6(fileName, (int)swDocumentTypes_e.swDocPART, (int)swOpenDocOptions_e.swOpenDocOptions_Silent, "", ref errors, ref warnings);
+                    }
+
+                    IModelDocExtension extMod = swModelDoc.Extension;
+
+                    extMod.Rebuild((int)swRebuildOptions_e.swForceRebuildAll);
+                    swModelDoc.Save3((int)swSaveAsOptions_e.swSaveAsOptions_UpdateInactiveViews, ref lErrors, ref lWarnings);
+                    swApp.CloseDoc(fileName);
+                    swModelDoc = null;
+
+                }
+            }
+            catch (System.Exception)
+            {
+                Debug.Print(errors.ToString());
+
+            }
+        }
+
+        void ListFormationToRebuild(FileRef fr)
+        {
+            if (fr.FileRefs == null) return;
+            foreach (FileRef item in fr.FileRefs)
+            {
+                if (item.FileRefs.Count > 0) ListFormationToRebuild(item);
+                if (list.Find(a => a.FileName == item.FileName) != null) continue;
+                list.Add(item);
+                // PrintTree(item);
+            }
+            list.Add(fr);
+        }
+
+        void PrintTree(FileRef fr)
+        {
+            Debug.Print(fr.FileName + '\n' + fr.ComponentLevel + '\n' + fr.PathName);
+        }
+
+
+        // The SldWorks swApp variable is pre-assigned for you.
+        public SldWorks swApp;
+    }
+}
---
+++ RebuildComponentSW/SwAddIn.cs
@@ -0,0 +1,120 @@
+﻿using CodeStack.SwEx.AddIn.Attributes;
+using CodeStack.SwEx.AddIn.Core;
+using CodeStack.SwEx.AddIn.Enums;
+using CodeStack.SwEx.AddIn.Helpers;
+using SolidWorksTools;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using SolidWorks.Interop.sldworks;
+using SolidWorks.Interop.swconst;
+using CodeStack.SwEx.AddIn.Base;
+using System.Xml.Serialization;
+using CodeStack.SwEx.Common.Attributes;
+using CodeStack.SwEx.AddIn.Delegates;
+using RebuildComponentSW.Properties;
+using CodeStack.SwEx.AddIn;
+
+namespace RebuildComponentSW
+{
+    [Title("CmdOpenPanel")]
+    [Description("CmdOpen")]
+    [Icon(typeof(Resources), nameof(Resources._01))]
+    [CommandGroupInfo(0)]
+    public enum Commands_e
+    {
+        [Title("CmdBuild")]
+        [Description("CmdBuildassemble")]
+        [Icon(typeof(Resources), nameof(Resources.command1_icon))]
+        CmdBuild
+
+      
+    }
+    public enum TaskPaneCommands_e
+    {
+        [Title("TaskPaneCnd")]
+        [Icon(typeof(Resources), nameof(Resources.command1_icon))]
+        CmdBuild   
+    }
+
+    [Guid("15CE97A5-D10D-4169-98F5-091DFEC7A2D9"), ComVisible(true)]
+    [AutoRegister("AddInSwRebuild", "AddInSWRebuldDoc", true)]
+    public class SwAddIn:SwAddInEx
+    {
+        private IDocumentsHandler<DocumentHandler> swDocsHandler;
+
+        public override bool OnConnect()
+        {
+             AddCommandGroup<Commands_e>(OnCommandClick, OnCommandEnable);
+
+
+            swDocsHandler = CreateDocumentsHandler();
+            swDocsHandler.HandlerCreated += OnHandlerCreated;
+            PanelTree ctrl;
+            var taskPaneView = CreateTaskPane<PanelTree, TaskPaneCommands_e>(OnTaskPaneCommandClick, out ctrl);
+
+            return true;
+        }
+
+        private void OnTaskPaneCommandClick(TaskPaneCommands_e cmd)
+        {
+            switch (cmd)
+            {
+                case TaskPaneCommands_e.CmdBuild:
+                    App.SendMsgToUser("TaskPane Command1 clicked!");
+                    break;
+
+            }
+        }
+
+        private void OnCommandEnable(Commands_e cmd, ref CommandItemEnableState_e state)
+        {
+            if (cmd == Commands_e.CmdBuild)
+            {
+                if (state == CommandItemEnableState_e.DeselectEnable)
+                {
+                    if (App.IActiveDoc2?.ISelectionManager?.GetSelectedObjectCount2(-1) == 0)
+                    {
+                        state = CommandItemEnableState_e.DeselectDisable;
+                    }
+                }
+            }
+        }
+
+        private void OnCommandClick(Commands_e cmd)
+        {
+            switch (cmd)
+            {
+                case Commands_e.CmdBuild:
+                    App.SendMsgToUser("Command1 clicked!");
+                    break;
+
+    
+            }
+        }
+
+        private void OnHandlerCreated(DocumentHandler handler)
+        {
+            handler.Activated += OnActivated;
+        }
+
+        private void OnActivated(DocumentHandler docHandler)
+        {
+            App.SendMsgToUser2($"'{docHandler.Model.GetTitle()}' activated",
+                (int)swMessageBoxIcon_e.swMbInformation, (int)swMessageBoxBtn_e.swMbOk);
+        }
+
+        private void OnDestroyed(DocumentHandler handler)
+        {
+            handler.Activated -= OnActivated;
+        }
+
+        public override bool OnDisconnect()
+        {
+            return base.OnDisconnect();
+        }
+    }
+}
